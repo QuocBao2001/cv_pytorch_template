e04e77091e53bf6027780d2d3b03fdb69970c8d7
Phan Quốc Bảo Nguyễn <zeronguyen2340@gmail.com>
2023-08-24 22:51:23 +0700

    add outline of trainer

---
+++ train.py
@@ -0,0 +1,24 @@
+class Attr_Trainer():
+    def __init__(self, config) -> None:
+        pass
+
+    def repare_data(self):
+        pass
+
+    def configure_optimization(self):
+        pass
+
+    def load_checkpoint(self):
+        pass
+
+    def save_model(self, save_best=False):
+        pass
+
+    def val_step(self):
+        pass
+
+    def compute_loss(self):
+        pass
+
+    def fit(self):
+        pass
